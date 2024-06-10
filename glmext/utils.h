9e5564653f2787411357154e9de6501c6da37883
Shane Morrison <shanemor@gmail.com>
2024-06-10 13:30:16 -0400

    fixes

--- glmext/utils.h
+++ glmext/utils.h
@@ -1,3 +1,4 @@
+#include <glm/gtc/quaternion.hpp> // glm::mat4
 
 namespace glm 
 {
@@ -11,23 +12,6 @@ template <typename T> qua<T> quat_from_axisangle(T angle, const vec<3, T> axis)
     result[1] = sin_half_angle * axis[0];
     result[2] = sin_half_angle * axis[1];
     result[3] = sin_half_angle * axis[2];
-
-    // should automagically be normalized (unit magnitude) now...
-    return result;
-}
-}
-namespace glm 
-{
-template <typename T> qua<T> QuatFromAA(T angle, const vec<3, T> axis)
-{
-    T half_angle = angle * (T)0.5;
-    T sin_half_angle = glm::sin(half_angle);
-
-    qua<T> result;
-    result[Welt] = glm::cos(half_angle);
-    result[Xelt] = sin_half_angle * axis[0];
-    result[Yelt] = sin_half_angle * axis[1];
-    result[Zelt] = sin_half_angle * axis[2];
 
     // should automagically be normalized (unit magnitude) now...
     return result;
